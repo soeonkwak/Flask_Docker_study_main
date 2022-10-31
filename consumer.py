d35c66394499c7423b249c2f81b693e5b3c0d94d
Soeon <rhkrthdjs@naver.com>
2022-11-01 02:11:00 +0900

    rabbitMQ 메세징3

--- consumer.py
+++ consumer.py
@@ -1,6 +1,9 @@
-import pika  # 이벤트를 보내는데 필요한 패키지
+import json  # 이벤트를 보내는데 필요한 패키지
+import pika
 
 # URL주소는 CloudAMQP에서 계정 생성하고 부여 받은 거.
+from main import Product, db
+
 params = pika.URLParameters('amqps://uuyqugbb:koSGNC-lwhlAm83l-uCIDy5g2SGJ5yBj@dingo.rmq.cloudamqp.com/uuyqugbb')
 
 # rabbitMQ로 연결 생성
@@ -14,7 +17,27 @@ channel.queue_declare(queue='main')
 
 def callback(ch, method, properties, body):
     print('Received in admin')
-    print(body)
+    data = json.loads(body)
+    print(data)
+
+    if properties.content_type == 'product_created':
+        product = Product(id=data['id'], title=data['title'], image=data['image'])
+        db.session.add(product)
+        db.session.commit()
+        print('Product Created')
+
+    elif properties.content_type == 'product_updated':
+        product = Product.query.get(data['id'])
+        product.title = data['title']
+        product.image = data['image']
+        db.session.commit()
+        print('Product Updated')
+
+    elif properties.content_type == 'product_deleted':
+        product = Product.query.get(data)
+        db.session.delete(product)
+        db.session.commit()
+        print('Product Deleted')
 
 
 channel.basic_consume(queue='main', on_message_callback=callback, auto_ack=True)
